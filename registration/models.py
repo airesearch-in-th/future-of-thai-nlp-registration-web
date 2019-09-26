180575b2154d283c0b8be6b1c8e387e7060889d0
Benjapol Worakan <benwrk@live.com>
2019-09-27 03:19:21 +0700

    Create model for participants

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Participant(models.Model):
+    registration_date_time = models.DateTimeField()
+    full_name = models.CharField(max_length=256)
+    email = models.EmailField(max_length=128, unique=True)
+    phone_number = models.CharField(max_length=64)
+    occupation = models.CharField(max_length=64)
+    position = models.CharField(max_length=64)
+    organization = models.CharField(max_length=256)
+    invitation_required = models.BooleanField()
+    checked_in = models.BooleanField(default=False)
